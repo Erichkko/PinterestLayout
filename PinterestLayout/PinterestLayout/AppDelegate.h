915c7376026f289c37f5c2e563dc0675221e1cc0
erichkko <erichkko@163.com>
2016-07-17 17:27:37 +0800

    init project
    初始化工程

---
+++ PinterestLayout/PinterestLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PinterestLayout
+//
+//  Created by wanglong on 16/7/17.
+//  Copyright © 2016年 wanglong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PinterestLayout/PinterestLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PinterestLayout
+//
+//  Created by wanglong on 16/7/17.
+//  Copyright © 2016年 wanglong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
